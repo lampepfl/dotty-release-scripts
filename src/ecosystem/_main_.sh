f36eef6736d76d8506bec7297ba53763469ac480
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-11-06 16:09:46 +0100

    Drop unused variables. Read scala version from file.

--- src/ecosystem/_main_.sh
+++ src/ecosystem/_main_.sh
@@ -2,7 +2,8 @@
 
 # Import Mechanism
   # https://stackoverflow.com/a/246128
-  ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/src/ecosystem"
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  ROOT_DIR="$SCRIPT_DIR/src/ecosystem"
   PROJECTS_DIR="$(pwd)/ecosystem_projects"
   PROJECT_SCRIPTS_DIR="$ROOT_DIR/projects"
   function load {
@@ -16,23 +17,8 @@
   load lib/workflow
 
 # Version-related variables
-  stable=${1:?Specify stable version as a first parameter to the script}
-  target_project_name=${2:?Specify target project as a second parameter to the script}
-
-  stable_patch=0
-  rc_patch=0
-  next_patch=0
-  rc_version=1
-
-  rc="$(($stable+1))"
-  next="$(($rc+1))"
-
-  stable_version="0.$stable.$stable_patch"
-  rc_version_preview="0.$rc.$rc_patch"
-  rc_version="$rc_version_preview-RC$rc_version"
-  next_version="0.$next.$next_patch"
-  stable_branch="0.$stable.x"
-  rc_branch="0.$rc.x"
+  rc_version=`cat $SCRIPT_DIR/release_version`
+  target_project_name=${1:?Specify target project as a second parameter to the script}
 
   TARGET_PROJECT="${target_project_name}.sh"
 
