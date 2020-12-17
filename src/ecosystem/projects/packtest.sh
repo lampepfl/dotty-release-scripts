38d87fc1a526eb998a8d204f2b5527fcac63889f
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-12-17 15:30:37 +0100

    Correctly handle versioning in packtest

--- src/ecosystem/projects/packtest.sh
+++ src/ecosystem/projects/packtest.sh
@@ -2,10 +2,10 @@
 
 function update_packtest {
   replace "artifacts" \
-    "zip=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$RC_PATTERN\/dotty-$RC_PATTERN.zip" \
-    "zip=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$release_version\/dotty-$release_version.zip"
+    "zip=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$RC_PATTERN\/scala3-$RC_PATTERN.zip" \
+    "zip=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$release_version\/scala3-$release_version.zip"
 
   replace "artifacts" \
-    "tar=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$RC_PATTERN\/dotty-$RC_PATTERN.tar.gz" \
-    "tar=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$release_version\/dotty-$release_version.tar.gz"
+    "tar=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$RC_PATTERN\/scala3-$RC_PATTERN.tar.gz" \
+    "tar=https:\/\/github.com\/lampepfl\/dotty\/releases\/download\/$release_version\/scala3-$release_version.tar.gz"
 }
