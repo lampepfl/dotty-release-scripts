50ca638cff2ae6eacb5f3218a3863f486cccdbb9
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-04-20 16:44:23 +0200

    Bump Coursier

--- src/ecosystem/projects/coursierapps.sh
+++ src/ecosystem/projects/coursierapps.sh
@@ -5,9 +5,22 @@ UPSTREAM_BRANCH_coursierapps="master"
 
 function update_coursierapps {
   replace "apps/resources/dotty-repl.json" \
-    "ch.epfl.lamp:dotty-compiler_[0-9\.]+:latest.stable" \
-    "org.scala-lang:scala3-compiler_$release_version:latest.stable"
-  replace "apps/resources/dotty-repl.json" \
-    "org.scala-lang:scala3-compiler_[0-9\.-M]+:latest.stable" \
-    "org.scala-lang:scala3-compiler_$release_version:latest.stable"
+    "org.scala-lang:scala3-compiler_$RC_PATTERN\:latest.release" \
+    "org.scala-lang:scala3-compiler_$release_version:latest.release"
+
+  replace "apps/resources/scala3-compiler.json" \
+    "org.scala-lang:scala3-compiler_$RC_PATTERN" \
+    "org.scala-lang:scala3-compiler_$release_version"
+
+  replace "apps/resources/scala3-decompiler.json" \
+    "org.scala-lang:scala3-compiler_$RC_PATTERN" \
+    "org.scala-lang:scala3-compiler_$release_version"
+
+  replace "apps/resources/scala3-doc.json" \
+    "org.scala-lang:scala3-doc_$RC_PATTERN" \
+    "org.scala-lang:scala3-doc_$release_version"
+
+  replace "apps/resources/scala3-repl.json" \
+    "org.scala-lang:scala3-compiler_$RC_PATTERN" \
+    "org.scala-lang:scala3-compiler_$release_version"
 }
