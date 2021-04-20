8b6faf076f225872cc0a76ec9ff41848dc110ada
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-04-20 17:01:25 +0200

    Migrate checklist to python

--- checklist.py
+++ checklist.py
@@ -1,13 +1,16 @@
-# #!/usr/bin/env bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-release_version=`cat $SCRIPT_DIR/release_version`
+#!/usr/bin/env python3
+import os
 
-LIST='- [ ] Finalize release
+script_dir = os.path.dirname(os.path.realpath(__file__))
+release_version = input('Version to be released: ')
+
+checklist = '''
+- [ ] Finalize release
   - [ ] Look at the milestone of the version being released. Move all the open issues from it to the next milestone.
   - [ ] Merge branch of the previous release into `master` to guarantee that all of the commits are propagated to `master`
 - [ ] Publish artifacts to Maven via CI
-  - [ ] Create branch `<release_version>` from `master`
-  - [ ] On that branch, set `baseVersion` and tag it as `<release_version>`
+  - [ ] Create branch `{release_version}` from `master`
+  - [ ] On that branch, set `baseVersion` and tag it as `{release_version}`
   - [ ] On `master`, set `baseVersion` to the next version to be released
 - [ ] Update `scalaVersion` (and, if applicable, the `sbt-dotty` version) in the Dotty ecosystem projects
   - [ ] https://github.com/scala/scala3-example-project [![Build Status](https://travis-ci.org/scala/scala3-example-project.svg?branch=master)](https://travis-ci.org/scala/scala3-example-project)
@@ -22,7 +25,7 @@ LIST='- [ ] Finalize release
   - [ ] https://github.com/scalacenter/scastie
     - [ ] PR submitted
     - [ ] PR merged
-    - [ ] https://scastie.scala-lang.org/ -> Build Settings -> Dotty mentions `<rc-version>`
+    - [ ] https://scastie.scala-lang.org/ -> Build Settings -> Dotty mentions `{release_version}`
   - [ ] Dotty reference compiler [![Dotty CI](https://github.com/lampepfl/dotty/workflows/Dotty%20CI/badge.svg?branch=master)](https://github.com/lampepfl/dotty/actions?query=branch%3Amaster)
     - [ ] PR submitted
     - [ ] PR merged
@@ -36,7 +39,8 @@ LIST='- [ ] Finalize release
   - [ ] Make an announcement thread on https://contributors.scala-lang.org
   - [ ] Tweet the announcement blog post on https://twitter.com/scala_lang
 
-[Instructions on how to release](https://dotty.epfl.ch/docs/contributing/procedures/release.html)'
+[Instructions on how to release](https://dotty.epfl.ch/docs/contributing/procedures/release.html)'''.format(
+  release_version = release_version,
+)
 
-echo "$LIST" |\
-  sed "s/<release_version>/$release_version/g"
+print(checklist)
